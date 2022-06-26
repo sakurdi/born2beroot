f0571861180db3984e714ca9dbee9d9a2ba7b65b
Your Name <you@example.com>
2022-06-27 00:19:04 +0200

    ram percentage

--- monitoring.sh
+++ monitoring.sh
@@ -4,3 +4,7 @@
 arch=`uname -a`
 phys_cpu=`grep "physical id" /proc/cpuinfo | sort | uniq | wc -l`
 virt_cpu_cores=`grep "processor" /proc/cpuinfo | wc -l`
+total_ram=`free -m | awk '$1 == "Mem:" {print $2}'`
+used_ram=`free -m | awk '$1 == "Mem:" {print $3}'`
+ram_percentage=`echo "scale=2; ($used_ram / $total_ram) * 100" | bc`
+echo $ram_percentage
